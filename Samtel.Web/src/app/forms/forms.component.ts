494057df440b93af9db8068080d12efb8d16c84a
Pedro Rivera <riverapetter@gmail.com>
2019-08-09 15:22:45 -0500

    Modificado valor por defecto de formulario

--- Samtel.Web/src/app/forms/forms.component.ts
+++ Samtel.Web/src/app/forms/forms.component.ts
@@ -25,10 +25,9 @@ export class FormsComponent implements OnInit {
         response = response.map(users => {
         users.edad = 1993;
         return users;
-      });
+        });
+        console.log("response", response)
         this.setData(response);
-
-   
       },
       error => {
         console.log("Error");
@@ -39,8 +38,8 @@ export class FormsComponent implements OnInit {
 
   loadComponent() {
     this.formPerson = this.FB.group({
-      nombre: new FormControl({ value: '', disable: false }, Validators.required),
-      apellido: new FormControl({ value: '', disable: false }, Validators.required)
+      nombre: new FormControl({ value: "o", disable: false }, Validators.required),
+      apellido: new FormControl({ value: "o", disable: false }, Validators.required)
     })
   }
 
